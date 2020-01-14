d5f7648467ede5a40ac929e02bfa3a1f89956c9b
--replace-all <ignaciobellomo@gmail.com>
2020-01-14 14:06:15 -0500

    Updates

---
+++ precache-manifest.63fd4b70bf9d6ec894f5895b889cea53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9d447c0ca0766637ab1",
+    "url": "/wiki-countries/static/css/main.32367908.chunk.css"
+  },
+  {
+    "revision": "e9d447c0ca0766637ab1",
+    "url": "/wiki-countries/static/js/main.e9d447c0.chunk.js"
+  },
+  {
+    "revision": "7cef243640a003abf550",
+    "url": "/wiki-countries/static/css/1.f95e7671.chunk.css"
+  },
+  {
+    "revision": "7cef243640a003abf550",
+    "url": "/wiki-countries/static/js/1.7cef2436.chunk.js"
+  },
+  {
+    "revision": "a459c7b8a46c1ff067a7",
+    "url": "/wiki-countries/static/js/runtime~main.a459c7b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wiki-countries/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ecaacba9ba1e2b70030d86588cfce4f8",
+    "url": "/wiki-countries/index.html"
+  }
+];
